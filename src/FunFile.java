482640ba3af94e6c31f5ef30b43b7d778463f016
MaxDeFarfalla <Maximilian.defarfalla@elev.ga.ntig.se>
2024-02-22 11:26:03 +0100

    Startade typ

---
+++ src/FunFile.java
@@ -0,0 +1,15 @@
+public class FunFile {
+
+    String name;
+
+    public FunFile(String name){
+        this.name = name;
+        FileCreate();
+    }
+
+
+    private void FileCreate() {
+
+    }
+
+}
